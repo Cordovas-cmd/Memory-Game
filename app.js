a1808bdf8bff0b91b74601eecfec67fee45f6498
samuel cordova <cordovas1337@gmail.com>
2022-06-25 05:39:16 -0500

    added some script to take an array of objects and randomly sort it with a math random and sort

---
+++ app.js
@@ -0,0 +1,61 @@
+// Variable to set up different cards
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    }
+]
+
+// console.log(cardArray)
+
+//sorts an array randomly
+/* Sort compares two values and then sorts through it , 
+this math random will return back a number anywhere from 0 to -1
+so here we are checking whether it's smaller than 0.5 or larger than 0.5 and shuffle it based on that.*/
+cardArray.sort(() => 0.5 - Math.random())
+
+console.log(cardArray)
